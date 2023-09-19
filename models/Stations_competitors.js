626cd468cd33cfa000b1a8633e9552832b678c62
Silvestre Emmanuel <silvestre.ramirez@wundertec.com>
2023-09-19 17:17:59 -0600

    added query to find competitors

--- models/Stations_competitors.js
+++ models/Stations_competitors.js
@@ -1,6 +1,23 @@
 const db = require("../config/db");
-const Stations_competitors = {
-  cre_id: 0,
-  cre_id_competitor: 0,
-  distance: 0,
+const Stations_competitors = {};
+
+Stations_competitors.getByStationId = (id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const conn = await db.getConnection();
+      const rows = await conn.query(
+        `SELECT  stations_competitors.cre_id_competitor, stations.name 
+            FROM stations_competitors 
+            INNER JOIN stations ON stations_competitors.cre_id_competitor = stations.cre_id 
+            WHERE stations_competitors.cre_id = ?`,
+        [id]
+      );
+      conn.end();
+      resolve(rows);
+    } catch (err) {
+      reject(err);
+    }
+  });
 };
+
+module.exports = Stations_competitors;
