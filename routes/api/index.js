4c3b25ff57c41f2128801cfb708c82e2284d50cc
Silvestre Emmanuel <silvestre.ramirez@wundertec.com>
2023-09-19 17:18:24 -0600

    added all the logic to the endpoint

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,14 +3,31 @@ const router = express.Router();
 const Stations = require("../../models/Stations");
 const Stations_brands = require("../../models/Stations_brands");
 const Prices = require("../../models/Prices");
+const getPriceDifferences = require("../../services/getPriceDifferences");
 
 router.get("/", async (req, res) => {
-  const id = "PL/1000/EXP/ES/2015";
-  console.log(await Stations.getById(id));
-  console.log(await Stations_brands.getByStationId(id));
-  console.log(await Prices.getByStationId(id));
+  const id = req.query.id;
+
+  const station = await Stations.getById(id);
+  const brands = await Stations_brands.getByStationId(id);
+  const prices = await Prices.getByStationId(id);
+
+  // Remove duplicate brands
+  const setOfBrands = [...new Set(brands.map((brand) => brand.name))];
+
+  const priceDifferences = await getPriceDifferences(id, prices);
   res.json({
-    message: "API - 👋🌎🌍🌏",
+    name: station.name,
+    location: `x: ${station.location_x}, y: ${station.location_y}`,
+    brands: setOfBrands,
+    prices: prices.map((price) => {
+      return {
+        price: price.value,
+        product: price.product,
+        date: price.date,
+      };
+    }),
+    priceDifferences: priceDifferences,
   });
 });
 module.exports = router;
